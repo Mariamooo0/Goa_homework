701272f40c438d4121d6ff7f5d286e34e530e817
Mariamooo0 <miqaberidzemari@gmail.com>
2024-05-20 21:01:00 +0400

    goa academy

---
+++ Day 04/homework.py
@@ -0,0 +1,43 @@
+# 1 davaleba  
+
+wigni = 10
+raodenoba = 5
+
+print(wigni*raodenoba)
+
+# 2 davaleba
+ 
+mari = 15
+taso = 15
+
+print("chemi asaki 10 wlis mere iqneba",mari + 10)
+print("chemi megobris asaki iqneba", taso + 10)
+ 
+# 3 davaleba 
+ 
+x = 10
+y = 20
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+# 4 davaleba
+
+name = "mari"
+age = 15
+
+print(name)
+print(age)
+
+# 5 davaleba 
+
+mari = 15 
+ana = 30
+
+print(ana-mari)
+
+
+
+
