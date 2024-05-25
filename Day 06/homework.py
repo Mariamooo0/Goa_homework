ba31f18a636c7cd3afece11c606953dde882efbd
Mariamooo0 <miqaberidzemari@gmail.com>
2024-05-25 20:34:08 +0400

    goa academy

---
+++ Day 06/homework.py
@@ -0,0 +1,48 @@
+#davaleba 1
+
+name = input("pls enter your name :")
+lastname = input ("enter your lastname")
+ 
+print(name + lastname)
+
+#davaleba 2 
+  
+x = 5  
+y  = int(input("enter number"))
+
+print(x + y)
+
+#davaleba 3
+
+x = 1
+y = 2
+z = 7
+
+print(x+y+z/3)
+ 
+#davaleba 4
+
+x = input ("enter name")
+
+print(x * 5)
+
+z = int (input("enter number"))
+
+print(z*5)
+
+# chven shevqmenit cvladi romelsac mivaniwet input punqcia da printis mnishvnelobit is gavamravlet 5ze 
+# amavdroulad shevqmenit meore cvladi romelic vaqciet intejerad radgan rogorc vicit strings ver gavamravlebt  intze
+
+#davaleba 5
+
+x = int(input("enter number"))
+y = int(input("enter number"))
+
+print(x*y)
+print(x/y)
+print(x+y)
+print(x-y)
+
+
+
+
