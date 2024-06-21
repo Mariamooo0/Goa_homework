04f6efd98c6433812adecb40928fe7cab1be1843
Mariamooo0 <miqaberidzemari@gmail.com>
2024-06-22 02:07:14 +0400

    forth commit

---
+++ Day 012/homework.py
@@ -0,0 +1,34 @@
+#1 davaleba
+
+# for i in range(20):
+#     print(i)
+
+#2 davaleba
+
+# num = int(input("pls enter a number :"))
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+#3 davaleba
+
+# for i in range(0,20,2):
+#     print(i)
+
+#4 davaleba
+
+# for i in range(50+100 ):
+#    print(i)
+
+#5 davaleba 
+
+# for num in range(1,50):
+#    if num % 5 == 0:
+#     print(num)
+
+#6 davaleba
+
+for i in range(5,10):
+    print(i)
+    
---
+++ Day 014/homework.py
@@ -0,0 +1,69 @@
+#while loop
+
+
+#1
+
+num = 1
+while num == 1:
+    print(num)
+
+
+
+#2
+
+num_2 = 2
+while num_2 == 2:
+    print(num_2)
+
+
+#3
+
+
+
+num_3 = 3
+while num_3 == 3:
+    print(num_3)
+
+
+#4
+
+num_4 = 4
+while num_4 == 4:
+    print(num_4)
+
+
+#5
+
+
+num_5 = 5
+while num_5 == 5:
+    print(num_5)
+
+
+
+
+
+
+# for loop
+
+#1
+
+for i in range(1,50):
+    print(i)
+
+
+#2
+
+for i in range(2,50):
+    print(i)
+
+
+#3
+
+for i in range(3,50):
+    print(i)
+
+#4
+
+for i in range(1,50):
+    print(i)
